a1dbefdbd2c72ae33b8bffc51beabe9016c961f4
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-08-09 11:11:40 -0300

    #08

---
+++ Assets/Scripts/Buttons/Buttons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    private PlayerMenu PlayerMenu;
+    void Start()
+    {
+        PlayerMenu = FindObjectOfType<PlayerMenu>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void UpdateEntrar()
+    {
+        StartCoroutine(LoadScene());
+        PlayerMenu.Move();
+    }
+
+    public void UpdateSair()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Scene 0");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenu : MonoBehaviour
+{
+    private Animator Animator;
+    private Vector2  NewPosition;
+    private bool     MoveNow;
+    void Start()
+    {
+        MoveNow  = false;
+        Animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if(MoveNow)
+        {   
+            NewPosition = new Vector2(20, -3);
+            transform.position = Vector2.MoveTowards(transform.position, NewPosition, 5 * Time.deltaTime);
+        }
+
+    }
+
+    public void Move()
+    {
+        Animator.SetBool("Run", true);
+        MoveNow     = true;
+        Destroy(gameObject, 5);
+    }
+}
