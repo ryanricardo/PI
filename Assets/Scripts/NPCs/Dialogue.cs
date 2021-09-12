db515243885a2a843c68db28695ec67667a5d471
Ryan <70291872+ryanricardo@users.noreply.github.com>
2021-09-12 16:26:55 -0300

    #14

--- Assets/Scripts/NPCs/Dialogue.cs
+++ Assets/Scripts/NPCs/Dialogue.cs
@@ -5,118 +5,121 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-    public enum States
+    public enum EStates
     {
-        NoTalking,
         Talking,
-    };
+        NoTalking,
+    }
 
-    [Header("Atributtes Dialogue")]
-    public                  string[]            TypesDialogues;
-    public                  TextMeshProUGUI     TextDialogue;
-    public                  bool                PlayDialogue;
-    public                  bool                SkippedStartDialogue;
-    private                 int                 CountDialogue; 
-    private                 int                 LimitCountDialogue;
+    public enum ETypeBots
+    {
+        NPCs,
+        Enemies,
+    }
+    
+
+
+    [Header("States")]
+    public  EStates             StatesCurrent;
+    public  ETypeBots           TypeBots;
+    [Header("Components")]
+    public  PlayerController    PlayerController;
+    public  TextMeshProUGUI     TextDialogue;
     [Header("Atributtes Distance")]
-    public                  float               distanceInteract;
-    private                 float               DistancePlayer;
-
-    public                  States              StatesCurrent;
-    public                  bool                OneTime;
+    public  float               DistancePlayer;
+    public  float               MinimiumDistance;
+    [Header("Atributtes Dialogues")]
+    public  string[]            TypesDialogues;
+    public  int                 CountDialogue;
+    public  int                 LimitCountDialogue;
+    public  bool                Play;
     
 
-    [HideInInspector]public PlayerController    PlayerController;
 
-
-    
-
-    
-
-    
 
     void Start()
     {
-        StatesCurrent       = States.NoTalking;
-        PlayerController    = FindObjectOfType<PlayerController>();    
+        PlayerController    = FindObjectOfType<PlayerController>();
         CountDialogue       = -1;
         LimitCountDialogue  = TypesDialogues.Length;
+        StatesCurrent       = EStates.NoTalking;
     }
 
-    
     void Update()
     {
-        switch(StatesCurrent)
+        Observer();
+        switch(TypeBots)
         {
-            case States.NoTalking:
-            Observer();
-            UpdateNoTalking();
+
+
+            case ETypeBots.NPCs:
+                switch(StatesCurrent)
+                {
+                    case EStates.NoTalking:
+
+                    if(DistancePlayer <= MinimiumDistance)
+                    StatesCurrent = EStates.Talking;
+
+                    break;
+
+                    case EStates.Talking:
+                    break;
+                }
             break;
-            case States.Talking:
-            Observer();
-            UpdateTalking();
+
+
+
+
+
+
+            case ETypeBots.Enemies:
+            
+                switch(StatesCurrent)
+                {
+                    case EStates.NoTalking:
+                        StopCoroutine(StartTalking());
+                        TextDialogue.gameObject.SetActive(false);
+                        if(DistancePlayer < MinimiumDistance)
+                        {
+                            StatesCurrent = EStates.Talking;
+                        }
+                        
+
+                    break;
+
+                    case EStates.Talking:
+                    TextDialogue.gameObject.SetActive(true);
+                    if(Play){StartCoroutine(StartTalking()); Play = false;}
+                    
+                    break;
+                }
             break;
         }
     }
+
+    // Functions
 
     void Observer()
     {
         DistancePlayer = Vector2.Distance(transform.position, PlayerController.transform.position);
     }
 
-    void UpdateNoTalking()
+    IEnumerator StartTalking()
     {
-        PlayerController.Moviment = true;
-        TextDialogue.gameObject.SetActive(false);
-        if(DistancePlayer <= distanceInteract & PlayerController.KeyCodeEDown 
-        || SkippedStartDialogue && DistancePlayer <= distanceInteract)
+        yield return new WaitForSeconds(3);
+        if(CountDialogue != LimitCountDialogue)
         {
-            StatesCurrent = States.Talking;
+            while(true)
+            {
+                yield return new WaitForSeconds(3);
+                CountDialogue += 1;
+                TextDialogue.text = TypesDialogues[CountDialogue];
+            }
+        }else if(CountDialogue >= LimitCountDialogue)
+        {
+            StatesCurrent = EStates.NoTalking;
         }
     }
-
-    void UpdateTalking()
-    {
-        
-        if(PlayDialogue)
-        {
-            PlayerController.Moviment = false;
-            TextDialogue.gameObject.SetActive(true);
-            StartCoroutine(Talking());
-            PlayDialogue = false;
-        }
-        if(OneTime)
-        {
-            FindObjectOfType<AudioController>().PlayMusic(0);
-            OneTime = false;
-        }else if(CountDialogue == LimitCountDialogue)
-        {
-            StatesCurrent = States.NoTalking;
-        }
-        
-        
-    }
-
-    IEnumerator Talking()
-    {
-        
-        while(CountDialogue < LimitCountDialogue)
-        {
-            yield return new WaitForSeconds(3);
-            CountDialogue += 1;
-            TextDialogue.text = TypesDialogues[CountDialogue];
-        }
-
-    }
-
-
-
-
-
-
 
     
-
-
-
 }
